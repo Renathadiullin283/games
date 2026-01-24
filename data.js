63010c40da34cbcc2b91d60241dad908f02e73cc
Renathadiullin283 <156587807+Renathadiullin283@users.noreply.github.com>
2026-01-24 13:27:18 +0300

    Update data.js

--- data.js
+++ data.js
@@ -16,11 +16,31 @@ export const CLASSES = {
 export const LOCATIONS = {
   factory: {
     name: "Заброшенный завод",
-    enemies: 10,
+    enemies: 5,
     enemyHp: 300,
+    enemyAtk: 20,
+    hpGrowth: 1.2,
+    atkGrowth: 1.1,
+    level: 1,
+  },
+  forest: {
+    name: "Лес",
+    enemies: 7,
+    enemyHp: 400,
     enemyAtk: 25,
     hpGrowth: 1.25,
     atkGrowth: 1.15,
+    level: 2,
+  },
+  dungeon: {
+    name: "Подземелье",
+    enemies: 10,
+    enemyHp: 600,
+    enemyAtk: 35,
+    hpGrowth: 1.3,
+    atkGrowth: 1.2,
+    level: 3,
   },
 };
+
 
