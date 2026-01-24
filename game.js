4d54c202870d6af25d9398843e7e625e70e2e1b8
Renathadiullin283 <156587807+Renathadiullin283@users.noreply.github.com>
2026-01-24 12:28:50 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,14 @@
-a
+// game.js
+import { CLASSES } from "./data.js";
+import { DEBUG_PLAYER } from "./debug.js";
+
+export const player = {
+  ...DEBUG_PLAYER,
+  maxHp: DEBUG_PLAYER.stats.hp,
+  currentHp: DEBUG_PLAYER.stats.hp,
+};
+
+export function resetHP() {
+  player.currentHp = player.maxHp;
+}
+
