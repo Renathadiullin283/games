8f5a27f744933984d682a72e4c43872638b31944
Renathadiullin283 <156587807+Renathadiullin283@users.noreply.github.com>
2026-01-24 12:27:55 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,25 @@
+// data.js
+export const CLASSES = {
+  warrior: {
+    name: "Воин",
+    hpMult: 1.5,
+    atkMult: 1.0,
+    defMult: 1.3,
+  },
+  assassin: {
+    name: "Ассасин",
+    hpMult: 1.0,
+    atkMult: 1.4,
+    critChance: 0.25,
+  },
+};
+
+export const LOCATIONS = {
+  factory: {
+    name: "Заброшенный завод",
+    enemies: 20,
+    baseEnemyHp: 120,
+    baseEnemyAtk: 12,
+  },
+};
 
