547a10fa87d2299f313fe5f3b44e0eb8359df1de
Renathadiullin283 <156587807+Renathadiullin283@users.noreply.github.com>
2026-01-24 12:28:24 +0300

    Update debug.js

--- debug.js
+++ debug.js
@@ -1,1 +1,13 @@
+// debug.js
+export const DEBUG_PLAYER = {
+  level: 50,
+  gold: 100000,
+  classId: "warrior",
+  stats: {
+    hp: 5000,
+    atk: 300,
+    def: 120,
+    crit: 0.15,
+  },
+};
 
